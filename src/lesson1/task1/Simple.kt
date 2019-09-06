6dbf6ccfff5e8685cd971ce51fb92da334231173
Kirill Timofeev <kirill.timofeev.a@gmail.com>
2019-09-07 00:36:22 +0300

    Lesson1 End

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -67,14 +67,14 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours * 3600 + minute
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes * 48 + arshins * 3 + vershoks) * 4.445 / 100
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes * 48 + arshins * 16 + vershoks) * 4.445 / 100
 /**
  * Тривиальная
  *
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (deg + ((min + sec / 60) / 60)) * PI / 180
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double = deg * PI / 180 + min * PI / (180 * 60) + sec * PI / (3600 * 180)
 
 /**
  * Тривиальная
@@ -97,7 +97,7 @@ fun thirdDigit(number: Int): Int = (number / 100) % 10
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = (hoursArrive - hoursDepart) * 60 + minutesDepart + minutesArrive
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = (hoursArrive - hoursDepart) * 60 - minutesDepart + minutesArrive
 /**
  * Простая
  *
@@ -105,7 +105,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int): Double = sqr(percent * 0.01 + 1) * (initial * percent * 0.01 + initial)
  /**
  * Простая
  *
